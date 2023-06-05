464ecb7f164bb080b981bfd502882d3642af4365
Chris <hakizachrist1@gmail.com>
2023-06-05 21:53:03 +0200

    Update migrations

---
+++ db/migrate/20230605153433_create_group.rb
@@ -0,0 +1,10 @@
+class CreateGroup < ActiveRecord::Migration[7.0]
+  def change
+    create_table :groups do |t|
+      t.string :name
+      t.references :icon_user, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :icon_entity, null: false, foreign_key: { to_table: :entities }, index: true
+      t.timestamps
+    end
+  end
+end
