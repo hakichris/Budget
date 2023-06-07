f7075b9d089236dfe43c308c598dfbb06207c003
Chris <hakizachrist1@gmail.com>
2023-06-07 23:36:44 +0200

    Update category

---
+++ app/models/category.rb
@@ -0,0 +1,16 @@
+class Category < ApplicationRecord
+  belongs_to :user
+  has_one_attached :icon
+  has_many :cat_transacts, dependent: :destroy
+  has_many :financial_transactions, through: :cat_transacts
+
+  validates :name, presence: true, length: { maximum: 250 }
+
+  def total(category_id)
+    financial_transactions.joins(:cat_transacts).where(cat_transacts: { category_id: }).sum('financial_transactions.amount')
+  end
+
+  def owner?(user)
+    self.user == user
+  end
+end
