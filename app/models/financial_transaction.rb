b7ae88db8e8f56ed4ec4613713fd0f3b37e70c7f
Chris <hakizachrist1@gmail.com>
2023-06-07 23:34:59 +0200

    Update financial_transactions

---
+++ app/models/financial_transaction.rb
@@ -0,0 +1,7 @@
+class FinancialTransaction < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :cat_transacts, dependent: :destroy
+  has_many :categories, through: :cat_transacts
+
+  validates :name, presence: true, length: { maximum: 250 }
+end
