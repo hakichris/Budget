294db10d891aa6826b117db71933520479d19450
Chris <hakizachrist1@gmail.com>
2023-07-31 16:50:39 +0200

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include Devise::Controllers::Helpers
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
+  end
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    new_user_session_path
+    puts "i am already here"
   end
 end
